470da2c14f4bd6eb1d19a733929cd7dc4d909787
M <doanm280@gmail.com>
2022-08-22 17:49:38 +0700

    m commit

--- src/route/web.js
+++ src/route/web.js
@@ -21,7 +21,7 @@ let initWebRouters = (app) => {
     router.put('/api/edit-user', userController.handleEditUser)
     router.delete('/api/delete-user', userController.handleDeleteUser)
 
-    router.get('/allcode', userController.getAllCode)
+    router.get('/api/allcode', userController.getAllCode)
 
     return app.use("/", router);
 }
